8864d25652d002130905ecb816753b4ee618adc2
fafkhe <115066487+fafkhe@users.noreply.github.com>
2025-08-13 11:42:00 +0330

    /

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,6 @@ import { Repository } from 'typeorm';
 import { createProject } from './dtos/createProject';
 import { Project } from './entities/project.entity';
 
-
 @Injectable()
 export class AppService {
   constructor(
@@ -13,31 +12,33 @@ export class AppService {
   ) {}
 
   async createProject(
-  body: createProject,
-  files: {
-    photo?: Express.Multer.File[];
-    logo?: Express.Multer.File[];
-  },
-): Promise<Project> {
-  const photoFile = files.photo?.[0];
-  const logoFile = files.logo?.[0];
+    body: createProject,
+    files: {
+      photo?: Express.Multer.File[];
+      logo?: Express.Multer.File[];
+    },
+  ): Promise<Project> {
+    const photoFile = files.photo?.[0];
+    const logoFile = files.logo?.[0];
 
-  const photoUrl = photoFile
-    ? `http://localhost:3000/uploads/${photoFile.filename}`
-    : '';
-  const logoUrl = logoFile
-    ? `http://localhost:3000/uploads/${logoFile.filename}`
-    : '';
+    console.log(createProject, '////// createProject');
 
-  const projectData = {
-    ...body,
-    photo: photoUrl,
-    logo: logoUrl,
-  };
+    const photoUrl = photoFile
+      ? `http://localhost:3000/uploads/${photoFile.filename}`
+      : '';
+    const logoUrl = logoFile
+      ? `http://localhost:3000/uploads/${logoFile.filename}`
+      : '';
 
-  const project = this.projectRepository.create(projectData);
-  return await this.projectRepository.save(project);
-}
+    const projectData = {
+      ...body,
+      photo: photoUrl,
+      logo: logoUrl,
+    };
+
+    const project = this.projectRepository.create(projectData);
+    return await this.projectRepository.save(project);
+  }
 
   async getAllProjects(): Promise<Project[]> {
     return await this.projectRepository.find();
@@ -47,13 +48,11 @@ export class AppService {
     try {
       const project = await this.projectRepository.findOne({ where: { id } });
       if (!project) {
-        throw new Error('Project not found'); 
+        throw new Error('Project not found');
       }
       return project;
     } catch (error) {
-
       console.log(error);
-      
     }
   }
 }
