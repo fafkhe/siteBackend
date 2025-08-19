d635f564cf959b52a689c930fd0c6b5a62e928a7
fafkhe <115066487+fafkhe@users.noreply.github.com>
2025-08-19 15:07:10 +0330

    .

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.enableCors({
-    origin: '*', // specify the allowed origin  
+  app.enableCors({
+    origin: '*', // specify the allowed origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // specify the allowed HTTP methods
-  })
+  }),
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true, 
+        whitelist: true, 
+        forbidNonWhitelisted: true,
+      }),
+    );
+
   // app.useStaticAssets(join(__dirname, '..', 'uploads'), {
   //   prefix: '/uploads/',
   // });
