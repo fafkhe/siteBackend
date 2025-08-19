816e429ac1494346386e7c34bc8a307ef7b443d2
fafkhe <115066487+fafkhe@users.noreply.github.com>
2025-08-19 14:01:08 +0330

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -17,6 +17,9 @@ export class AppService {
 
   async createProject(createProductDto: createProject) {
     try {
+
+      console.log(createProductDto,"//////////////inja");
+      
       const project = this.projectRepository.create(createProductDto);
       return await this.projectRepository.save(project);
     } catch (error) {
